2481c7d7087e4f97922ce11c1a73eafe9917f2e5
vaishnav bhagat <bhagatvaishnav07@gmail.com>
2026-01-27 10:44:46 +0530

    ls

--- frontend/src/routes/Approutes.jsx
+++ frontend/src/routes/Approutes.jsx
@@ -3,22 +3,14 @@ import { NavLink, Route, Routes } from "react-router-dom";
 import Signin from "../components/Signin";
 import Login from "../components/Login";
 import Home from "../components/Home";
-import ProtectedRoute from "../components/PrivateRoutes";
+
 
 const Approutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Signin />} />
       <Route path="/login" element={<Login />} />
-      <Route
-        path="/home"
-        element={
-          <ProtectedRoute>
-            {" "}
-            <Home />
-          </ProtectedRoute>
-        }
-      />
+      <Route path="/home" element={<Home />} />
     </Routes>
   );
 };
